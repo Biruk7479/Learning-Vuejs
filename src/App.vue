1b742d4af197c8aed9d48d2a49fd7c2c90a499ad
Biruk Ayalew <Biruka2580@gmail.com>
2024-10-27 18:59:32 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data(){
+    return{
+      name:'Jon Doe',
+      status: 'active'
+    }
+  }
+}
+</script>
+
+<template>
+ <h1>{{name}}</h1>
+ <p v-if ="status === 'active'">user is active</p>
+ <p v-else-if ="status === 'pending'">user is active</p>
+ <p v-else>user is inactive</p>
+</template>
